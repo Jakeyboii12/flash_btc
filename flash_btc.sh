befe168c68ad9bc9382957c8fdf53f98038326ca
Jakeyboii12 <141609937+Jakeyboii12@users.noreply.github.com>
2024-02-17 16:48:28 +0000

    Create flash_btc.sh
    flash BTC

---
+++ flash_btc.sh
@@ -0,0 +1,6 @@
+#this a flash generator btc whats send to binamce coinbase and trustwallet and many more 
+#max send per day 1btc 
+#software still in development we would like surport from serious buyers 
+#must be able to run use linux oh termux more support we get we can move further and bring it to the market online 
+
+best flash btc and esy to use 
